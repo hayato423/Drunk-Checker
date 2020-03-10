917657a2647f8ac9b35d90509c204e8f0b382c02
hayato423 <49159244+hayato423@users.noreply.github.com>
2020-03-10 22:29:51 +0900

    計算を修正、表示の変更
    血中アルコール濃度の計算式を修正。
    結果画面に総摂取アルコール量を表示。

--- routes/index.js
+++ routes/index.js
@@ -13,12 +13,13 @@ router.get('/', function(req, res, next) {
 let weight = 0;
 let alcohol_g = 0
 let sum_alcohol_g = 0
-const coefficient = 0.8   //アルコール比重
+let sum_drink_quantity = 0;
 
 router.get('/result',function(req,res,next) {
   weight = req.query.weight;
   req.session.weight = weight;
   data = {
+    sum_alcohol_g : 0,
     blood_alcohol_concentration : 0
   }
   res.render('result',data);
@@ -27,17 +28,22 @@ router.get('/result',function(req,res,next) {
 
 router.post('/result',function(req,res,next) {
   var percent = Number(req.body['percent']);
-  var quantity = req.body['quantity'];
-  alcohol_g = quantity * percent / 100 * coefficient;
-  console.log(percent + " : " + quantity + " : " + alcohol_g);
+  var drink_quantity = req.body['quantity'];
+  //純アルコール量を計算
+  alcohol_g = drink_quantity * percent / 100 * 0.8;
+  //合計アルコール量(g)を計算
   if(req.session.sum_alcohol_g != undefined){
     sum_alcohol_g = req.session.sum_alcohol_g + alcohol_g;
   }else{
     sum_alcohol_g = alcohol_g;
   }
   req.session.sum_alcohol_g = sum_alcohol_g;
-  var blood_alcohol_concentration = sum_alcohol_g / req.session.weight * 0.75;
+  //最高血中アルコール濃度を計算
+  var blood_alcohol_concentration = sum_alcohol_g / (req.session.weight*1000*0.66) * 100
+  //小数第２位で四捨五入
+  blood_alcohol_concentration = Math.round(blood_alcohol_concentration * 100) / 100
   var data = {
+    sum_alcohol_g : sum_alcohol_g,
     blood_alcohol_concentration : blood_alcohol_concentration
   };
   res.render('result',data);
